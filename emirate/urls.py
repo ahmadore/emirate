e22b636e97c56aae379f31a06536452169c64e3b
Ahmad Bello <pylather@gmail.com>
2018-03-23 10:36:06 +0000

    m

--- emirate/urls.py
+++ emirate/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     url(r'^blog/', include('blog.urls')),
@@ -12,6 +13,7 @@ urlpatterns = [
     url(r'^contact/', views.contact),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns += staticfiles_urlpatterns()
