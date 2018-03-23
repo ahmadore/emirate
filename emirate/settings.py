f8b2584a84e58b5baa9eb8c289937a4f88e85280
Ahmad Bello <pylather@gmail.com>
2018-03-23 10:51:03 +0000

    m

--- emirate/settings.py
+++ emirate/settings.py
@@ -125,3 +125,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, '../static/')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATICFILES_DIR = [
+    '/home/ubuntu/emirate/static',
+]
