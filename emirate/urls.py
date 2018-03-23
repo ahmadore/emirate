557bddb184a28cea90ea262390a15091ea25e296
Ahmad Bello <pylather@gmail.com>
2018-03-23 11:29:09 +0000

    static files fixed

--- emirate/urls.py
+++ emirate/urls.py
@@ -3,7 +3,6 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     url(r'^blog/', include('blog.urls')),
@@ -13,7 +12,6 @@ urlpatterns = [
     url(r'^contact/', views.contact),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-urlpatterns += staticfiles_urlpatterns()
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
